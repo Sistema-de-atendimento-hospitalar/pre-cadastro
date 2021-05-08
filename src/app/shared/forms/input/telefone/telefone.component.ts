c2d044e24fd6eb55348322f090cfea67339909cb
stefbitt <stefany.recode01@gmail.com>
2021-05-08 17:54:51 -0300

    alteracao

--- src/app/shared/forms/input/telefone/telefone.component.ts
+++ src/app/shared/forms/input/telefone/telefone.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TipoTelefoneService } from 'src/app/service/generico/tipo-telefone.service';
+import { PacienteService } from 'src/app/service/paciente/paciente.service';
+import { Paciente } from 'src/models/paciente.model';
 import { Telefone } from 'src/models/telefone.model';
 import { TipoTelefone } from 'src/models/tipoTelefone.model';
 
@@ -11,14 +13,34 @@ import { TipoTelefone } from 'src/models/tipoTelefone.model';
 export class TelefoneComponent implements OnInit {
 
   @Input() telefone: Telefone;
+  @Input() telefones: Telefone[];
+  @Input() indice: number;
+  @Input() showDeleteOption: boolean;
+  private paciente: Paciente;
   tiposTelefone: TipoTelefone[];
 
-  constructor(private tipoTelefoneService: TipoTelefoneService) { }
+  constructor(private tipoTelefoneService: TipoTelefoneService, private pacienteService: PacienteService) { }
 
   ngOnInit(): void {
     this.tipoTelefoneService.getTipoTelefone().subscribe(result => {
       this.tiposTelefone = result
     });
+    this.paciente = this.pacienteService.getPacienteFromLocalStore();
+  }
+
+  deleteTelefone(telefone: Telefone) {
+    if (this.telefones.length == 1) {
+      alert('Não pode remover o único contato\nSe preferir pode alterar os dados!')
+      return false;
+    }
+
+    if(telefone.telefoneId) {
+      this.pacienteService.deleteTelefone(telefone, this.paciente).subscribe(result => {
+        alert('Exclusão efetuada com sucesso!')
+      });
+    }
+
+    this.telefones.splice(this.indice, 1);
   }
 
 }
